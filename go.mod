846ca839a757a716eaf5f121b64024f27698b4fb
Logan <60761520+Preloading@users.noreply.github.com>
2024-09-03 22:09:10 -0600

    Basic auth

--- go.mod
+++ go.mod
@@ -5,24 +5,25 @@ go 1.22.2
 require (
 	github.com/gofiber/contrib/websocket v1.3.2
 	github.com/gofiber/fiber/v2 v2.52.5
-	github.com/mikedev101/cloudlink/golang v0.0.0-20240528001032-1a94e9daf3f4
 )
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/bwmarrin/snowflake v0.3.0 // indirect
 	github.com/fasthttp/websocket v1.5.8 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gofiber/contrib/jwt v1.0.10 // direct
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.52.0 // indirect
+	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,101 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/json"
 	"log"
 	"runtime/debug"
 	"time"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/golang-jwt/jwt"
 
-	cloudlink "github.com/mikedev101/cloudlink/golang/server"
+	// Auth
+
+	jwtware "github.com/gofiber/contrib/jwt"
+)
+
+type LoginRequest struct {
+	Username string
+	Password string
+}
+
+var (
+	// Obviously, this is just a test example. Do not do this in production.
+	// In production, you would have the private key and public key pair generated
+	// in advance. NEVER add a private key to any GitHub repo.
+	privateKey *rsa.PrivateKey
 )
 
 func main() {
 	// Configure runtime settings
 	debug.SetGCPercent(35) // 35% limit for GC
 
+	// MOTD
+	motd := "Welcome to scratchcord!"
+
+	// Auth setup
+	rng := rand.Reader
+	var err error
+	privateKey, err = rsa.GenerateKey(rng, 2048)
+	if err != nil {
+		log.Fatalf("rsa.GenerateKey: %v", err)
+	}
+
 	// Create fiber application
 	app := fiber.New()
 
-	// Create CloudLink server
-	manager := cloudlink.New("root")
+	// app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+	}))
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		// Variable is only valid within this handler
+		return c.SendString("Hello, World!")
+	})
 
-	// Modify configuration of the CloudLink manager
-	manager.Config.CheckIPAddresses = false
-	manager.Config.EnableMOTD = true
-	manager.Config.MOTDMessage = "Welcome to ScratchCord!"
+	app.Post("/login", func(c *fiber.Ctx) error {
+		r := new(LoginRequest)
+
+		if err := json.Unmarshal(c.BodyRaw(), &r); err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		// Create the Claims
+		claims := jwt.MapClaims{
+			"username": r.Username,
+			"exp":      time.Now().Add(time.Hour * 72).Unix(),
+		}
+		// Create token
+		token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString(privateKey)
+		if err != nil {
+			log.Printf("token.SignedString: %v", err)
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.JSON(fiber.Map{"token": t, "motd": motd})
+	})
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{
+			JWTAlg: jwtware.RS256,
+			Key:    privateKey.Public(),
+		},
+	}))
+
+	app.Get("/check_auth", func(c *fiber.Ctx) error {
+		user := c.Locals("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+		name := claims["name"].(string)
+		return c.SendString("Welcome " + name)
+	})
 
 	// Add a websocket path
 	app.Use("/ws", func(c *fiber.Ctx) error {
@@ -39,13 +109,41 @@ func main() {
 	})
 
 	// Bind CloudLink server to websocket path
-	app.Get("/ws", websocket.New(func(client *websocket.Conn) {
-		cloudlink.SessionHandler(client, manager)
+	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
+		// c.Locals is added to the *websocket.Conn
+		log.Println(c.Locals("allowed"))  // true
+		log.Println(c.Params("id"))       // 123
+		log.Println(c.Query("v"))         // 1.0
+		log.Println(c.Cookies("session")) // ""
+
+		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write:", err)
+				break
+			}
+			if err = c.WriteMessage(mt, []byte("Hello this is websockets!")); err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+
 	}))
 
 	app.Get("/monitor", monitor.New(
 		monitor.Config{
-			Title:   "CloudLink Metrics",
+			Title:   "Metrics",
 			Refresh: (50 * time.Millisecond),
 		},
 	))
