05c72d139ce2dd67ee7887af69ad8d7fbe73ef01
Logan <60761520+Preloading@users.noreply.github.com>
2024-10-05 10:32:37 -0600

    fix: nudges

--- discord-integration.go
+++ discord-integration.go
@@ -17,14 +17,44 @@ func start_discord_webhook() {
 
 			user := Accounts{}
 			db.First(&user, "id = ?", msg.data.UserId)
-
+			// 1 - Normal Message
+			// 2 - Nudge
+			// 3 - Typing, does not store in DB
+			// 4 - Game Start Request
+			// 100 - Global Message (admin only)
+			// 101 - Global TTS Message (admin only)
+			// 102 - Channel Special Message (admin only)
+			// 103 - Channel Special TTS Message (admin only)
+			// 104 - Kick User (admin only)
+			// 105 - Kick All Users (admin only)
+			var webhookUsername string
+			var webhookContents string
+			switch msg.data.Type {
+			case 1:
+				webhookUsername = user.Username
+				webhookContents = msg.data.Message
+			case 2:
+				webhookUsername = user.Username
+				webhookContents = user.Username + " has sent a nudge!"
+			case 100:
+			case 101:
+			case 102:
+			case 103:
+				webhookUsername = "System Message"
+				webhookContents = user.Username + ": " + msg.data.Message
+			case 3:
+			case 4:
+			case 104:
+			case 105:
+				continue
+			}
 			message := discordwebhook.Message{
-				Username: &user.Username,
+				Username: &webhookUsername,
 				//AvatarUrl: &user.Avatar,
-				Content: &msg.data.Message,
+				Content: &webhookContents,
 			}
 			if err := discordwebhook.SendMessage(webhook_url, message); err != nil {
-				log.Fatal(err)
+				log.Println(err)
 			}
 		}
 	}()
